e64dbac9d248d95429d18010d76f533c2c4a505c
LaurenSky <ljherrer@gmail.com>
2016-09-27 16:29:20 -0700

    added task list and erb to tasks/index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,13 @@
 class TasksController < ApplicationController
   def index
+    @all_tasks = [
+      {name: "Groceries", description: "apples, oranges, pasta, veggies", completion_status: "incomplete", completion_date: nil},
+      {name: "Appointment", description: "eye docotor on thursday", completion_status: "incomplete", completion_date: nil},
+      {name: "call jane", description: "for her birthday", completion_status: "completed", completion_date: "9/25/16"},
+      {name: "oil change", description: "car needs one by end of month", completion_status: "incomplete", completion_date: nil}
+    ]
+  end
+
+  def show
   end
 end
