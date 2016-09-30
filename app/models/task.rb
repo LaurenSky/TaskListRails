189e1d73f026ffd67094a3f84292a1a9e5930ce1
LaurenSky <ljherrer@gmail.com>
2016-09-30 14:47:52 -0700

    dry code by adding methods to the task.rb file

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,15 @@
 class Task < ActiveRecord::Base
+  def date_view
+    @date_completed = created_at.strftime("%m/%d/%Y")
+    return @date_completed
+  end
+
+  def task_completed?
+    if completed_at != nil
+      true
+    else
+      false
+    end
+  end
+
 end
